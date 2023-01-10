2f7ac56e7c369cd82b57a0dcdeb3409925698be2
Vaibhav <vaibhavmonu8309@gmail.com>
2023-01-10 11:19:55 +0530

    feat: add dynamic og tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,33 +1,63 @@
-import { useRouter } from 'next/router'
-import React from 'react'
+import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
+import React from "react";
+
+const description =
+  "Welcome to the Kizzy documentation site! Here you'll find comprehensive guides and documentation to help you get started with the Kizzy app, as well as troubleshooting resources and reference materials. Explore the various sections of the site to learn more about the features and functionality of the Kizzy app.";
+
+const ogImageApi = (title: string) =>
+  `https://docs-dead8309.vercel.app/api/og?title=${title}`;
 
 export default {
   useNextSeoProps() {
-    const { route } = useRouter()
-    if (route !== '/') {
+    const { route } = useRouter();
+    if (route !== "/") {
       return {
-        titleTemplate: '%s – Docs'
-      }
+        titleTemplate: "%s – Docs",
+      };
     }
   },
-  head: (<>
-    <link rel='shortcut icon' href='/icon.png' />
-    <meta
-      property='og:description'
-      content="Welcome to the Kizzy documentation site! Here you'll find comprehensive guides and documentation to help you get started with the Kizzy app, as well as troubleshooting resources and reference materials. Explore the various sections of the site to learn more about the features and functionality of the Kizzy app."
-    />
-  </>),
+  head: () => {
+    const { asPath } = useRouter();
+    const { title } = useConfig();
+    console.log(useRouter())
+
+    return (
+      <>
+        <link rel="shortcut icon" href="/icon.png" />
+        <meta property="og:description" content={description} />
+        <meta
+          property="og:url"
+          content={`https://docs-dead8309.vercel.app${asPath}`}
+        />
+        <meta property="og:image" content={ogImageApi(title)} />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:site" content="Kizzy Docs" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:card" content={title} />
+        <meta name="twitter:image:src" content={ogImageApi(title)} />
+      </>
+    );
+  },
   project: {
-    link: 'https://github.com/dead8309/Kizzy',
+    link: "https://github.com/dead8309/Kizzy",
   },
   chat: {
-    link: 'https://discord.gg/vUPc7zzpV5',
+    link: "https://discord.gg/vUPc7zzpV5",
   },
-  docsRepositoryBase: 'https://github.com/dead8309/kizzy-docs', 
+  docsRepositoryBase: "https://github.com/dead8309/kizzy-docs",
   logo: (
     <>
-      <img width="32" height="32" src="./icon.png" style={{borderRadius: '8px'}}/>
-      <span style={{ marginLeft: '.4em', fontWeight: 600, fontSize: '150%' }}>
+      <img
+        width="32"
+        height="32"
+        src="./icon.png"
+        style={{ borderRadius: "8px" }}
+      />
+      <span style={{ marginLeft: ".4em", fontWeight: 600, fontSize: "150%" }}>
         Kizzy
       </span>
     </>
@@ -40,6 +70,4 @@ export default {
   nextThemes: {
     defaultTheme: "dark",
   },
-}
-
-
+};
